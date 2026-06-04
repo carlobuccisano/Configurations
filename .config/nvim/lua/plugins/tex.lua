194bf29cb0846824ee7d3c58dca54d59da7b05d0
Carlo Buccisano <6238019+carlobuccisano@users.noreply.github.com>
2026-06-04 14:55:49 +0200

    Added tex.lua plugin to nvim

---
+++ .config/nvim/lua/plugins/tex.lua
@@ -0,0 +1,25 @@
+return {
+  -- VimTeX
+  { 
+    'lervag/vimtex', 
+    lazy = false,
+	enabled = false,
+    init = function()
+      vim.g.vimtex_view_method = 'zathura'
+      vim.g.vimtex_complete_close_braces = 1
+	  vim.opt.conceallevel = 2
+    end
+  },
+
+  -- UltiSnips
+  { 
+    'SirVer/ultisnips',
+	enabled = false,
+    init = function()
+      vim.g.UltiSnipsExpandTrigger = '<Tab>'
+      vim.g.UltiSnipsJumpForwardTrigger = '<Tab>'
+      vim.g.UltiSnipsJumpBackwardTrigger = '<S-Tab>'
+      vim.g.UltiSnipsSnippetDirectories = { vim.fn.expand('$HOME') .. '/.config/nvim/UltiSnips' }
+    end
+  },
+}
